227fac01663d11c7611a9214ce21c5d468db4156
Aniket Rao <aniketprogdev@gmail.com>
2021-03-23 12:08:08 +0530

    added a transaction blueprint

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() {
   runApp(MyHomePage());
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Onions',
+      amount: 120.32,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Shoes',
+      amount: 250.12,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
